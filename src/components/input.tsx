fdde09a52e84cc31d84f78cc8c8d1661df601bf7
Alan Strauhs <alanstrauhs@madebysauce.com>
2024-02-06 17:54:37 +0000

    refactor: use builtin react type for handling input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,22 +1,15 @@
 import { FormEvent } from "react";
 
-type InputProps = {
-  type: string;
-  name: string;
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   label: React.ReactNode | string;
-  // could change this to take in a string to display error text underneath the field
   error?: boolean;
-  value?: string;
-  onChange?: (event: FormEvent<HTMLInputElement>) => void | null;
-};
+  onChange: (event: FormEvent<HTMLInputElement>) => void | null;
+}
 
 export default function Input({
-  type,
-  name,
   label,
-  value,
-  onChange,
   error = false,
+  ...inputProps
 }: InputProps) {
   return (
     <label className="block text-sm font-medium leading-6 text-gray-900">
@@ -26,10 +19,7 @@ export default function Input({
           className={`border-0 block w-full rounded-md py-1.5 shadow-sm ring-inset
           focus:ring-2 focus:ring-inset outline-none sm:text-sm sm:leading-6
           ${error ? "ring-2 ring-red focus:ring-red-dark" : "ring-1 ring-gray-300 focus:ring-green"}`}
-          type={type}
-          name={name}
-          onChange={onChange}
-          value={value}
+          {...inputProps}
         />
       </div>
     </label>
